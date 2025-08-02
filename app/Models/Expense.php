4341e7fa22357b2b6753f301ff819f5a740f2646
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-02 12:20:50 +0700

    crud expense dan payment method

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    use HasFactory;
+
+    //wajib untuk kolom yang bisa diisi
+    protected $fillable = [
+        'name',
+        'note',
+        'date_expense',
+        'amount',
+    ];
 }
