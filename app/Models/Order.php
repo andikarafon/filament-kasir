769bafc38dc15170ff8004eea02ff9f00eccc078
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-02 11:55:07 +0700

    update relasi tabel

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    public function paymentMethod(): BelongsTo
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
 }
