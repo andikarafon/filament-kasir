1b0954259f706408bcc6e3c32223bbe96f5b89d7
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-03 18:41:17 +0700

    CRUD Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,14 +5,43 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+            'name',
+            'slug',
+            'category_id',
+            'stock',
+            'price',
+            'is_active',
+            'image',
+            'barcode',
+            'description',
+    ];
+
     //1 produk hanya punya 1 kategori
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Category::class);
+    }
+
+    public static function generateUniqueSlug(string $name): string
+    {
+        //Jika ada slug yang sama, maka akan ada counter di belakangnya
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while (self::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
+
     }
 }
