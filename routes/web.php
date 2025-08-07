e34b9a85cf5945ef29b78273113b6b7cb816ed70
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-07 12:05:01 +0700

    template excel done

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-
+use App\Exports\TemplateExport;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Facades\Excel;
 
 
 Route::get('/', function () {
@@ -10,3 +11,7 @@ Route::get('/', function () {
         ? redirect()->route('filament.admin.pages.dashboard')
         : redirect()->route('filament.admin.auth.login');
 });
+
+Route::get('/download-template', function() {
+    return Excel::download(new TemplateExport, 'template.xlsx');
+})->name('download-template');
