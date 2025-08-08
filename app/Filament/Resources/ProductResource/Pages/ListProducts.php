9515430e0c5f4cc024e873dda343cd9acc28d606
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-08 17:12:24 +0700

    import excel produk

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -4,9 +4,13 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\Action;
 use Filament\Forms\Components\FileUpload;
-use Filament\Resources\Pages\ListRecords;
+use App\Imports\ProductImport;
+use Maatwebsite\Excel\Facades\Excel;
+use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Session;
 
 class ListProducts extends ListRecords
 {
@@ -16,17 +20,44 @@ class ListProducts extends ListRecords
     {
         return [
             Action::make('importProducts')
-                ->label('Import Product')
-                ->icon('heroicon-s-arrow-down-tray')
-                ->color('danger')
-                ->form([
-                    FileUpload ::make('attachment')
-                        ->label('Upload Template Produk')
-                ]),
+                    ->label('Import Product')
+                    ->icon('heroicon-s-arrow-down-tray')
+                    ->color('danger')
+                    ->form([
+                        FileUpload::make('attachment')
+                            ->label('Upload Template Product')
+                    ])
+                    ->action(function (array $data) {
+                        $file = public_path('storage/'. $data['attachment']);
+
+                        try {
+                            Excel::import(new ProductImport, $file);
+                            Notification::make()
+                                ->title('Product imported')
+                                ->success()
+                                ->send();
+                        } catch(\Exception $e) {
+                            dd($e);
+                            Notification::make()
+                                    ->title('Product failed to import')
+                                    ->danger()
+                                    ->send();
+                        }
+                    }),
             Action::make("Download Template")
-                    ->url(route('download-template'))
-                    ->color('success'),
+                ->url(route('download-template'))
+                ->color('success'),
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function setFlashMessage()
+    {
+        $error = Session::get('error');
+
+        if ($error) {
+            $this->notify($error, 'danger');
+            Session::forget('error');
+        }
     }
 }
