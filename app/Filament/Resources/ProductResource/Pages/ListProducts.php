c413e298712c955ba912906f6c39d3a7783fecbd
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-08 10:57:06 +0700

    UI Import Product

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
@@ -13,6 +15,17 @@ class ListProducts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('importProducts')
+                ->label('Import Product')
+                ->icon('heroicon-s-arrow-down-tray')
+                ->color('danger')
+                ->form([
+                    FileUpload ::make('attachment')
+                        ->label('Upload Template Produk')
+                ]),
+            Action::make("Download Template")
+                    ->url(route('download-template'))
+                    ->color('success'),
             Actions\CreateAction::make(),
         ];
     }
