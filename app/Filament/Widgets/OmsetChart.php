51de99d6998975b931ec9eec5329753afa76d201
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-11 15:28:17 +0700

    Omset Chart Done

--- app/Filament/Widgets/OmsetChart.php
+++ app/Filament/Widgets/OmsetChart.php
@@ -3,23 +3,90 @@
 namespace App\Filament\Widgets;
 
 use Filament\Widgets\ChartWidget;
-
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Order;
+use Carbon\Carbon;
 class OmsetChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
-
-    protected static ?int $sort = 1; //agar ada di bawah widget yang 0 (statistik)
+    protected static ?string $heading = 'Omset';
+    protected static ?int $sort = 1;
+    public ?string $filter = 'today';
+    protected static string $color = 'success';
 
     protected function getData(): array
     {
+        $activeFilter = $this->filter;
+
+        $dateRange = match ($activeFilter) {
+            'today' => [
+                'start' => now()->startOfDay(),
+                'end' => now()->endOfDay(),
+                'period' => 'perHour'
+            ],
+            'week' => [
+                'start' => now()->startOfWeek(),
+                'end' => now()->endOfWeek(),
+                'period' => 'perDay'
+            ],
+            'month' => [
+                'start' => now()->startOfMonth(),
+                'end' => now()->endOfMonth(),
+                'period' => 'perDay'
+            ],
+            'year' => [
+                'start' => now()->startOfYear(),
+                'end' => now()->endOfYear(),
+                'period' => 'perMonth'
+            ]
+        };
+
+
+        $query = Trend::model(Order::class)
+            ->between(
+                start: $dateRange['start'],
+                end: $dateRange['end'],
+            );
+
+        if ($dateRange['period'] === 'perHour') {
+            $data = $query->perHour();
+        } elseif ($dateRange['period'] === 'perDay') {
+            $data = $query->perDay();
+        } else {
+            $data = $query->perMonth();
+        }
+
+        $data = $data->sum('total_price');
+
+        $labels = $data->map(function (TrendValue $value) use ($dateRange) {
+            $date = Carbon::parse($value->date);
+
+            if ($dateRange['period'] === 'perHour') {
+                return $date->format('H:i');
+            } elseif ($dateRange['period'] === 'perDay') {
+                return $date->format('d M');
+            } 
+            return $date->format('M Y');
+        });
+    
         return [
-                'datasets' => [
-                    [
-                        'label' => 'Blog posts created',
-                        'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
-                    ],
+            'datasets' => [
+                [
+                    'label' => 'Omset '.$this->getFilters()[$activeFilter],
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
                 ],
-                'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            ],
+            'labels' => $labels,
+        ];
+    }
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'Today',
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
         ];
     }
 
