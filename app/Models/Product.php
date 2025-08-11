bff0b260553343e1e21d5ca98c76569b6c487145
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-11 16:11:07 +0700

    Table Product Favorite

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Product extends Model
@@ -54,4 +55,10 @@ class Product extends Model
     {
         $query->where("name", "like", "%{$value}%");
     }
+
+    public function orderProducts(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+
 }
