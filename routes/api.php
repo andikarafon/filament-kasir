6d8a1ae1bbdbef55d65cd616dbb3ecb57c85e784
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-08-14 14:15:16 +0700

    api login done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Models\User;
+
+route::post('login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
